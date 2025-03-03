60e50d28c0cf18530b876d837dac51defdec58fc
w.alexander <will_alexander@me.com>
2025-03-03 10:07:51 +0100

    Remove boilerplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
+  template: ``,
   styles: [],
 })
 export class AppComponent {
-  title = 'observables-course';
 }
