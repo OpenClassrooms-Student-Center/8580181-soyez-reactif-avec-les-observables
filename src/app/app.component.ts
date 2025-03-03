737c0fa889c72a2e874ad2b32bbee52af3e2bb35
w.alexander <will_alexander@me.com>
2025-03-03 10:19:21 +0100

    Add main title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  template: ``,
+  template: `
+    <h1 class="text-4xl text-orange-500 text-center my-12">Soyez réactif avec les Observables</h1>
+		<router-outlet />`,
   styles: [],
+  imports: [
+    RouterOutlet
+  ]
 })
 export class AppComponent {
 }
