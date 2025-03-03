5483f72b90532710d323b6a16762a18a27781803
w.alexander <will_alexander@me.com>
2025-03-03 22:53:10 +0100

    Add message to chapter 2, auto-update nav dropdown on initial load

--- src/app/chapitres/chapitre-2/home.component.ts
+++ src/app/chapitres/chapitre-2/home.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   imports: [],
   template: `
     <h1 class="text-2xl text-gray-600">Créez un Observable et souscrivez-y</h1>
+    <p class="bg-orange-100 text-gray-600 w-fit mt-3 mx-auto py-1 px-3">Ouvrez la console de votre navigateur</p>
   `,
   styles: ``
 })
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { filter, tap } from 'rxjs';
+import { delay, filter, take, tap } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -11,7 +11,7 @@ import { filter, tap } from 'rxjs';
 			<h1 class="text-4xl text-orange-500 mt-12 mb-6">Soyez réactif avec les Observables</h1>
 			<nav>
 				<select class="border-2 border-gray-300 p-1 text-gray-600" [formControl]="routeCtrl">
-          <option disabled value="">-- Veuillez sélectionner un chapitre --</option>
+					<option disabled value="">-- Veuillez sélectionner un chapitre --</option>
 					<option value="create-an-observable">Créez un Observable et souscrivez-y</option>
 					<option value="basic-operators">Manipulez les émissions avec les opérateurs bas niveau</option>
 					<option value="unsubscribe-strategies">Évitez les fuites de mémoire avec des stratégies de unsubscribe
@@ -27,19 +27,35 @@ import { filter, tap } from 'rxjs';
   ]
 })
 export class HeaderComponent {
-  routeCtrl = new FormControl<string>('');
+  routeCtrl: FormControl<string>;
 
   private router = inject(Router);
 
   constructor() {
+    this.routeCtrl = new FormControl<string>('', { nonNullable: true });
     this.navigateOnRouteCtrlChange();
+    this.setCtrlValueOnInitialLoad();
+  }
+
+  private setCtrlValueOnInitialLoad() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      take(1),
+      delay(0),
+      tap(event => {
+        this.routeCtrl.setValue(this.routePathFor(event), { emitEvent: false });
+      })
+    ).subscribe();
+  }
+
+  private routePathFor(event: NavigationEnd) {
+    return event.url.split('/')[1];
   }
 
   private navigateOnRouteCtrlChange() {
     this.routeCtrl.valueChanges.pipe(
       takeUntilDestroyed(),
-      filter(route => route !== null),
-      tap(route => this.router.navigateByUrl(route))
+      tap(route => this.router.navigate([route]))
     ).subscribe();
   }
 }
