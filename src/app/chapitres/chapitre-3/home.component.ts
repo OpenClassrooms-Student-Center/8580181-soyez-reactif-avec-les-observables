ce031d986fc4457b8739a619b13c59167d9353f4
w.alexander <will_alexander@me.com>
2025-03-03 23:00:09 +0100

    Add interval$ to chapter 3

--- src/app/chapitres/chapitre-3/home.component.ts
+++ src/app/chapitres/chapitre-3/home.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-basic-operators',
-  imports: [],
+  imports: [
+    AsyncPipe
+  ],
   template: `
 		<h1 class="text-2xl text-gray-600">Manipulez les émissions avec les opérateurs bas niveau</h1>
+		<p class="text-xl mt-3 text-orange-600 font-bold">{{ interval$ | async }}</p>
   `,
   styles: ``
 })
 export class HomeComponent {
-
+  interval$ = interval(1000);
 }
