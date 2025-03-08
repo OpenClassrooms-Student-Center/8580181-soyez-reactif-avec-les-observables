ac6d7a6272e621f49d152d91fdb09769afbf1d40
w.alexander <will_alexander@me.com>
2025-03-08 11:27:24 +0100

    End of chapter

--- src/app/chapitres/chapitre-4/home.component.ts
+++ src/app/chapitres/chapitre-4/home.component.ts
@@ -1,22 +1,35 @@
 import { Component } from '@angular/core';
-import { interval, tap } from 'rxjs';
+import { interval, take, tap } from 'rxjs';
 import { ChapterTitleComponent, ConsoleTipComponent } from '@ui-elements';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-unsubscribe-strategies',
   imports: [
     ConsoleTipComponent,
-    ChapterTitleComponent
+    ChapterTitleComponent,
+    AsyncPipe
   ],
   template: `
     <app-chapter-title>Évitez les fuites de mémoire avec des stratégies de unsubscribe</app-chapter-title>
 		<app-console-tip/>
+    <p>Stops at 5: {{ stopsAt5$ | async }}</p>
+    <p>Stops when component destroyed: {{ stopsOnDestroy$ | async }}</p>
   `,
   styles: ``
 })
 export class HomeComponent {
   interval$ = interval(1000).pipe(
     tap(console.log)
+  );
+
+  stopsAt5$ = interval(1000).pipe(
+    take(6)
+  );
+
+  stopsOnDestroy$ = this.interval$.pipe(
+    takeUntilDestroyed()
   );
 
   constructor() {
